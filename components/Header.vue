200279d3ae3705d05aa6c4e6b4d3943727d08a18
ozamorowski <ozamorowski@gmail.com>
2020-06-17 16:35:52 +0200

    Header component.

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="uk-navbar-container uk-margin" uk-navbar>
+    <div class="uk-navbar-left">
+      <div class="uk-navbar-item uk-logo" href="#">Where in the world?</div>
+    </div>
+    <div class="uk-navbar-right">
+      <div class="uk-navbar-item">
+        <button class="uk-button uk-button-default" uk-icon="paint-bucket">
+          Dark mode
+        </button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
