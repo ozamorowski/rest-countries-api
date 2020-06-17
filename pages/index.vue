ab73b5a2b49bc0254866e0fa4947ffa444467d0d
ozamorowski <ozamorowski@gmail.com>
2020-06-17 13:50:32 +0200

    Add UIkit and search form.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="container">
+    <form class="uk-search uk-search-large">
+      <span uk-search-icon></span>
+      <input
+        class="uk-search-input"
+        type="search"
+        v-model="search"
+        placeholder="Search..."
+      />
+    </form>
     <ul>
-      <li v-for="{ alpha3Code, name } of countries" :key="alpha3Code">
+      <li v-for="{ alpha3Code, name } of filteredCountries" :key="alpha3Code">
         <nuxt-link :to="{ name: 'country-code', params: { code: alpha3Code } }">
           {{ name }}
         </nuxt-link>
@@ -15,7 +24,8 @@ import axios from 'axios'
 
 export default {
   data: () => ({
-    countries: []
+    countries: [],
+    search: ''
   }),
   mounted() {
     this.$nextTick(async () => {
@@ -24,6 +34,13 @@ export default {
       this.countries = res.data
       this.$nuxt.$loading.finish()
     })
+  },
+  computed: {
+    filteredCountries() {
+      return this.countries.filter(country =>
+        country.name.toLowerCase().includes(this.search.toLowerCase())
+      )
+    }
   }
 }
 </script>
