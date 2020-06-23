063905c337cc547d7283429b60b106e5aca9b41c
ozamorowski <ozamorowski@gmail.com>
2020-06-23 17:01:21 +0200

    Remove dark mode switch button.

--- components/Header.vue
+++ components/Header.vue
@@ -3,13 +3,6 @@
     <div class="uk-navbar-left">
       <div class="uk-navbar-item uk-logo" href="#">Where in the world?</div>
     </div>
-    <div class="uk-navbar-right">
-      <div class="uk-navbar-item">
-        <button class="uk-button uk-button-default" uk-icon="paint-bucket">
-          Dark mode
-        </button>
-      </div>
-    </div>
   </nav>
 </template>
 
