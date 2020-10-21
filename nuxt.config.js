3f22e5767d4ee84398178daaf9f0b245f0774324
ozamorowski <ozamorowski@gmail.com>
2020-10-21 16:30:53 +0200

    Use vuex-persist to store state in browser local storage.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,10 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/lazyload.js'],
+  plugins: [
+    '@/plugins/lazyload.js',
+    { src: '~/plugins/vuex-persist', ssr: false }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
